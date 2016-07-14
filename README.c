90f4989f6f56ee62fc64a7ef03af44d3a0bf4e34
kirithikabaskar <click2kirithika@gmail.com>
2016-07-14 14:58:42 +0530

    Create README.c

---
+++ README.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n ;
+printf("enter the number");
+scanf("%d",n);
+if(n%2==0)
+{
+printf("is even number");}
+else
+{
+printf(" is odd number");
+}
+}
+}
